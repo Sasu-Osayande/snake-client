32991523007c28f8545c5c732d45e9ccd3a6dbe5
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-16 16:10:54 -0500

    Updated variable names

--- client.js
+++ client.js
@@ -11,20 +11,22 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   // messgae when connected to server
-  conn.on("connect", (connection) => {
+  conn.on("data", (message) => {
     console.log("Successfully connected to game server");
-    connection;
-  })
+    message;
+  });
 
   // initials for snake
-  conn.write("Name: SAO");
+  conn.on("connect", () => {
+    conn.write("Name: SAO");
+  });
   
   // conn.write("Move: up");
 
   // handle incoming data and print message to player
-  conn.on("data", (data) => {
-    console.log(data);
-  })
+  // conn.on("data", (data) => {
+  //   console.log(data);
+  // })
 
   return conn;
 };
