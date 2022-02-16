3d30f10582eacfae2d413e88b005c9483f77f92a
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-16 17:38:04 -0500

    Created a file to contain constants that can be exported and used by other files

---
+++ constants.js
@@ -0,0 +1,33 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+
+const EXIT = '\u0003';
+
+const MOVE_UP_KEY = '\u0077';
+const MOVE_LEFT_KEY = '\u0061';
+const MOVE_DOWN_KEY = '\u0073';
+const MOVE_RIGHT_KEY = '\u0064';
+
+// const SNAKE = '\u0070';
+// const LAUGH = '\u0069';
+// const BYE = '\u006F'
+
+// const SAYINGS = {
+// SNAKE: "Say: ssss",
+// LAUGH: "Say: teehee",
+// BYE: "Say: ttyl",
+// };
+
+module.exports = {
+  IP,
+  PORT,
+  EXIT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  // SNAKE,
+  // LAUGH,
+  // BYE,
+  // SAYINGS,
+};
