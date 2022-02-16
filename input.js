42a57e088c3b75ea606ed73cf41f65725ff88390
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-16 16:10:29 -0500

    Established keyboard commands for movements

--- input.js
+++ input.js
@@ -1,18 +1,34 @@
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
-const handleUserInput = function () {
+const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === '\u0077') {
+    connection.write("Move: up");
+  }
+  if (key === '\u0061') {
+    connection.write("Move: left");
+  }
+  if (key === '\u0073') {
+    connection.write("Move: down");
+  }
+  if (key === '\u0064') {
+    connection.write("Move: right");
+  }
+}
+
+stdin.on("data", handleUserInput);
+  return stdin;
 };
 
 module.exports = { setupInput };
